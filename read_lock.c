6dffd72fde1e5ffebe2a5593fa2349f95089b402
JIE483216300 <59871477+wangjie483216300@users.noreply.github.com>
2020-03-20 20:16:18 +0800

    Add files via upload

---
+++ read_lock.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "set_lock.c"
+int main(void){
+		int fd;	
+		fd = open("hello", O_RDWR | O_CREAT, 0644);
+		if(fd < 0){
+			printf("打开文件失败\n");			
+			exit(1);
+		}
+		/* 给文件上写入锁 */
+		set_lock(fd, F_RDLCK);
+	 	getchar();
+	    /* 给文件解锁 */
+	    set_lock(fd, F_UNLCK);	
+	    getchar();
+		close(fd);
+		return 0;
+}
