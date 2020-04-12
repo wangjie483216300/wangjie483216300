ce9e5b08ce089a129448846857691471cd41cce2
JIE483216300 <59871477+wangjie483216300@users.noreply.github.com>
2020-04-12 19:19:29 +0800

    Add files via upload

--- zuoyetestA.c
+++ zuoyetestA.c
@@ -16,38 +16,39 @@ int main (){
     sem_id = getsem();
     //对信号量初始化
     init_sem(sem_id,0);
-   /**----------------准备操作-----------------start-----*/
-        char buff1[1024];//缓冲区1
-        char buff2[1024];//缓冲区2
-        int shmid1,shmid2;//共享内存标识符
-        char* shm_addr1;//映射共享内存1
-        char* shm_addr2;//映射共享内存2
-        //创建共享内存--shmid为共享内存标识符
-        shmid1 = shmget(ftok(".",'a'),1024,0666);//1写
-        //映射共享内存
-        shm_addr1 = shmat(shmid1,0,0);
-          //创建共享内存--shmid为共享内存标识符
-        shmid2 = shmget(ftok(".",'b'),1024,0666);//2读
-        //映射共享内存
-        shm_addr2 = shmat(shmid2,0,0); 
- /**----------------准备操作-----------------end-----*/  
+	char buff1[1024];//缓冲区1
+	char buff2[1024];//缓冲区2
+	int shmid1,shmid2;//共享内存标识符
+	char* shm_addr1;//映射共享内存1
+	char* shm_addr2;//映射共享内存2
+	//创建共享内存--shmid为共享内存标识符
+	shmid1 = shmget(ftok(".",'a'),1024,0666);//1写
+	//映射共享内存
+	shm_addr1 = shmat(shmid1,0,0);
+	  //创建共享内存--shmid为共享内存标识符
+	shmid2 = shmget(ftok(".",'b'),1024,0666);//2读
+	//映射共享内存
+	shm_addr2 = shmat(shmid2,0,0); 
     while(1){
 
  /**----------------发送数据---------start-----*/ 
   /**写*/memset(buff1,0,1024);//清空缓冲区
         printf("请您输入:\n");
-        fgets(buff1,1023,stdin);
-        strncpy(shm_addr1,buff1,1024);//2共享内存复制到shm_addr1
+        if(fgets(buff1,1023,stdin)<0)
+			perror("fgets");
+        strncpy(shm_addr1,buff1,sizeof(buff1));//2共享内存复制到shm_addr1
         //判断内容
-        if(strncmp(buff1,"over",5)==0){
+        if(strncmp(buff1,"over",4)==0){
             //写入完毕,v操作,打开B的读取        
             sem_v(sem_id);
+            sleep(1);
             printf("关闭聊天!\n");
-            exit(-1);        
+			break;
+ //           exit(-1);        
         }else{
              //写入完毕,v操作,打开B的读取        
              sem_v(sem_id);
-             sleep(0.1);//睡0.1秒,确保B能使用到资源
+             sleep(1);//睡1秒,确保B能使用到资源
              memset(buff1,0,1024);//清空缓冲区             
         }      
  /**----------------发送数据---------end-----*/  
@@ -59,31 +60,24 @@ int main (){
  /**读*/memset(buff2,0,1024);//清空缓冲区
         strncpy(buff2,shm_addr2,1024);//2共享内存复制到buff
         //判断内容
-        if(strncmp(buff2,"over",5)==0){
+        if(strncmp(buff2,"over",4)==0){
             printf("对方不想理你了,并关闭了聊天!\n");
-            exit(-1);        
+//            exit(-1);        
+			break;
         }else{
-             printf("小姐姐B对你说:%s\n",buff2); 
+             printf("小姐姐B对你说:\n%s",buff2); 
              memset(buff2,0,1024);//清空缓冲区            
         }  
  /**----------------接受数据---------end-----*/ 
 
 
-
-
-    memset(shm_addr1,0,1024);
-    memset(shm_addr2,0,1024);
-
-
      }
-
-
-
-    /**----------------结束操作--------------*/ 
-        shmdt(shm_addr1);//解除映射
-        shmdt(shm_addr2);//解除映射
-        shmctl(shmid1,IPC_RMID,NULL);//删除共享内存
-        shmctl(shmid2,IPC_RMID,NULL);//删除共享内存
+    
+/**----------------结束操作--------------*/ 
+	shmdt(shm_addr1);//解除映射
+	shmdt(shm_addr2);//解除映射
+	shmctl(shmid1,IPC_RMID,NULL);//删除共享内存
+	shmctl(shmid2,IPC_RMID,NULL);//删除共享内存
     return 0;
 }
 
--- zuoyetestB.c
+++ zuoyetestB.c
@@ -5,7 +5,8 @@ int main (){
    
     int sem_id;//信号量标识符
     //获取信号量
-    sem_id = getsem();
+//    sem_id = getsem();
+	sem_id=semget(ftok(".",'a'),1,0666|IPC_CREAT);
     //对信号量初始化
     init_sem(sem_id,0);
    /**----------------准备操作-----------------start-----*/
@@ -15,35 +16,34 @@ int main (){
         char* shm_addr1;//映射共享内存
         char* shm_addr2;//映射共享内存
         //创建共享内存--shmid为共享内存标识符
-        shmid1 = shmget(ftok(".",'a'),1024,0666);//读
+        shmid1 = shmget(ftok(".",'a'),1024,0666|IPC_CREAT);//读
         //映射共享内存
         shm_addr1 = shmat(shmid1,0,0);
           //创建共享内存--shmid为共享内存标识符
-        shmid2 = shmget(ftok(".",'b'),1024,0666);//写
+        shmid2 = shmget(ftok(".",'b'),1024,0666|IPC_CREAT);//写
         //映射共享内存
         shm_addr2 = shmat(shmid2,0,0); 
- /**----------------准备操作-----------------end-----*/ 
-   
-
-
-
- while(1){
+ /**----------------准备操作-----------------end-----*/   
+    while(1){
  /**----------------接受数据---------start-----*/  
         //p操作--等待A写入之后读取
  /**读*/sem_p(sem_id); 
         memset(buff1,0,1024);//清空缓冲区
         strncpy(buff1,shm_addr1,1024);//2共享内存复制到buff
         //判断内容
-        if(strncmp(buff1,"over",5)==0){
+        if(strncmp(buff1,"over",4)==0){
 
             printf("对方不想理你了,并关闭了聊天!\n");
-            exit(-1);        
+//            exit(-1); 
+			break;       
         }else{
             
-             printf("小哥哥A对你说:%s\n",buff1); 
+             printf("小哥哥A对你说:\n%s",buff1); 
              memset(buff1,0,1024);//清空缓冲区            
         }
  /**----------------接受数据---------end-----*/ 
+
+
 
 
 
@@ -54,25 +54,31 @@ int main (){
         strncpy(shm_addr2,buff2,1024);//2共享内存复制到shmid2
    
         //判断内容
-        if(strncmp(buff2,"over",5)==0){
-             sleep(0.1);
+        if(strncmp(buff2,"over",4)==0){
+ //            sleep(1);
              sem_v(sem_id);//打开v操作,让A执行
+            sleep(1);
             printf("关闭聊天!\n");
-            exit(-1);        
+//            exit(-1);        
+			break;
         }else{
-             sleep(0.1);
              sem_v(sem_id);//打开v操作,让A执行
+             sleep(1);
              memset(buff2,0,1024);//清空缓冲区             
         }
 /**----------------发送数据---------end-----*/
-    memset(shm_addr1,0,1024);
-    memset(shm_addr2,0,1024);
-     }
-/**----------------结束操作--------------*/       
-    shmdt(shm_addr1);//解除映射
-    shmdt(shm_addr2);//解除映射
-    shmctl(shmid1,IPC_RMID,NULL);//删除共享内存
-    shmctl(shmid2,IPC_RMID,NULL);//删除共享内存
+  
+
+
+
+
+
+/**----------------结束操作--------------*/     
+	}  
+	shmdt(shm_addr1);//解除映射
+	shmdt(shm_addr2);//解除映射
+	shmctl(shmid1,IPC_RMID,NULL);//删除共享内存
+	shmctl(shmid2,IPC_RMID,NULL);//删除共享内存
     return 0;
 }
 
